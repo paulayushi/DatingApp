668cdcaefd08b5547833e4b658b49a6efb6f8ae9
Paul <spaul162@cable.comcast.com>
2019-04-04 12:40:41 -0400

    Intial Commit - DatingApp

---
+++ DatingApi.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApi.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApi.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DatingApi.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DatingApi.API.Models
+{
+    public class Value
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
