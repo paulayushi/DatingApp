9d80de2e6de8e4de06660d42d99182c2eb89901c
Paul <spaul162@cable.comcast.com>
2019-05-22 19:58:34 -0400

    Changes for deployment

--- DatingApi.API/Startup.cs
+++ DatingApi.API/Startup.cs
@@ -85,7 +85,14 @@ namespace DatingApi.API
             //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc( routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
